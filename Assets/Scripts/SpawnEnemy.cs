d5f6fb56edfd82eea3ad1f07e76da833bcc6c6f6
possumdraws <benmurphyart@gmail.com>
2026-03-26 15:13:38 -0400

    Squashed commit of the following:
    commit ae6f821d7f1197ab969a3c6d4285bb7847740721
    Author: possumdraws <benmurphyart@gmail.com>
    Date:   Thu Mar 26 15:03:10 2026 -0400
    
        Cube Orange
    
    commit a67100735bec0fc158778ced41d0c4a553880356
    Author: possumdraws <benmurphyart@gmail.com>
    Date:   Thu Mar 26 01:21:26 2026 -0400
    
        Cube Green
    
    commit c915c8e55fc2ffe286cef877aa6a023df49ea747
    Author: possumdraws <benmurphyart@gmail.com>
    Date:   Thu Mar 26 01:20:10 2026 -0400
    
        Made Cube Pink
    
    commit 374a84b38679a4e30402b4f35d71768044667606
    Author: possumdraws <benmurphyart@gmail.com>
    Date:   Tue Mar 24 19:34:18 2026 -0400
    
        Screen Arrows & Summander
    
        -Adjusted Screen Arrows
        -Added Summander Prefab
    
    commit 1fbc5d0e34d2dc59447a0aabeb7e12dcaff98eca
    Merge: 5d18397 d451f62
    Author: possumdraws <benmurphyart@gmail.com>
    Date:   Tue Mar 24 18:32:16 2026 -0400
    
        Merge branch 'SpawnerBranch' of https://github.com/possumdraws/Hostile-Variables-Repo into SpawnerBranch
    
    commit 5d183974fe1a4426b8dc439485f15a58db44ac9e
    Author: possumdraws <benmurphyart@gmail.com>
    Date:   Tue Mar 24 18:32:12 2026 -0400
    
        Added Summando
    
        -Added Summando enemy
        -Move spawner slightly higher
    
    commit d451f628b7abf2c654e06cba1d4dfc45ad90a5b3
    Author: Slowlayer-dev <kmontarti@icloud.com>
    Date:   Tue Mar 24 18:31:12 2026 -0400
    
        CAMERA OVERHAUL
    
        - Back to 3-pt view. Added appropriate UI.
        - Required a but of blocking out code lines, but
           I'll neaten it up later
    
    commit 8a26f9652c9a27511bdbd0fa3ae93a9eaaface1d
    Author: possumdraws <benmurphyart@gmail.com>
    Date:   Fri Mar 20 20:02:21 2026 -0400
    
        Laser Sound
    
        -Added New Laser Sound
        -Changed Floor Color

--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -1,16 +1,18 @@
-using System.Collections;
-using Unity.VisualScripting;
-//using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
-
-public class SpawnEnemy : MonoBehaviour
+using System.Collections;
+using System.ComponentModel;
+public class SpawnEnemies_Random : MonoBehaviour
 {
     private Transform spawnerPosition;
     [Header("Enemy Prefab (Addet)")]
-    public GameObject addetEnemy;
+    public GameObject[] basicEnemies;
+    public GameObject[] heavyEnemies;
+    private int pickEnemy;
     private GameObject spawnedAddetEnemy;
     private bool canSpawn;
+    [SerializeField]
     private int spawned = 0;
+
     [Header("How Many Can Spawn Before it Stops")]
     public int maxSpawned = 5;
 
@@ -19,18 +21,22 @@ public class SpawnEnemy : MonoBehaviour
     {
         //get location of this spawner in the world
         spawnerPosition = transform;
+
     }
 
-    // Update is called 60x per second
-    void Update()
+    // Update is called every frame
+    void FixedUpdate()
     {
+        pickEnemy = Random.Range(0, basicEnemies.Length);
+
         //if the in game enemy exists, we cannot spawn another one.
         if (spawnedAddetEnemy == null && maxSpawned > spawned && !canSpawn)
         {
             //This just means that it wont activate until the yield is complete
             StartCoroutine(Spawn());
         }
-        
+
+
     }
 
     //IEnumerator is just unity stuff for delays. its a return type
@@ -40,8 +46,15 @@ public class SpawnEnemy : MonoBehaviour
 
         //wait for however many seconds we set this to (I did 3 for now)
         yield return new WaitForSeconds(3f);
+
+        //check that enemy prefab exists
+        if (basicEnemies[pickEnemy] == null)
+        {
+            Debug.LogError("Enemy prefab is missing");
+            yield break;
+        }
         //spawned enemy = Instantiate(addet prefab, spawner gameobject pos, rot)
-        spawnedAddetEnemy = Instantiate(addetEnemy, spawnerPosition.position, spawnerPosition.rotation);
+        spawnedAddetEnemy = Instantiate(basicEnemies[pickEnemy], spawnerPosition.position, spawnerPosition.rotation);
 
         //add so it stops spawning enemies eventually
         spawned++;
