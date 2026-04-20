bd3a35ee5273fc66fa716f80b6cf526e10a4ede6
Slowlayer-dev <kmontarti@icloud.com>
2026-04-20 15:22:22 -0400

    Small script change for attacks
    getting a baseline down. shouldnt be hard, just crammed rn.

--- Assets/Enemies/EnemyScripts/EnemyAttacks.cs
+++ Assets/Enemies/EnemyScripts/EnemyAttacks.cs
@@ -2,11 +2,27 @@ using UnityEngine;
 
 public class EnemyAttacks : MonoBehaviour
 {
+    /* 
+     * SPAWNERS
+     * ROW 0
+     * ROW 1 (ranger)
+     * ROW 2 (basic)
+     * 
+     * PLAYER
+     */
+
     EnemyProblem enemyProblem;
     public Transform playerTarget;
 
     [Header("Animator")]
     public Animator animator;
+
+    struct Attack
+    {
+        public int damage;
+        public int range;
+
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,21 +38,29 @@ public class EnemyAttacks : MonoBehaviour
 
     void BasicAttack()
     {
-
+        Attack attack = new Attack();
+        attack.damage = 5;
+        attack.range = 2;
     }
 
     void HeavyAttack()
     {
-
+        Attack attack = new Attack();
+        attack.damage = 10;
+        attack.range = 2;
     }
 
     void RangedAttack()
     {
-
+        Attack attack = new Attack();
+        attack.damage = 3;
+        attack.range = 1;
     }
 
     void SpecialAttack()
     {
-
+        Attack attack = new Attack();
+        attack.damage = 1;
+        attack.range = 2;
     }
 }
